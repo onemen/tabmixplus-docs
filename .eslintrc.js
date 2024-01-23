4ced8666776e667e5513ebf8d324cb36565e4b71
onemen <tabmix.onemen@gmail.com>
2024-01-23 07:29:41 +0200

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+
+  parserOptions: {
+    ecmaVersion: 2023,
+    sourceType: 'script',
+    requireConfigFile: false,
+  },
+
+  overrides: [
+    {
+      // All .eslintrc.js files are in the node environment, so turn that
+      // on here.
+      // https://github.com/eslint/eslint/issues/13008
+      files: ['.eslintrc.js', '*.config.js'],
+      env: {
+        node: true,
+        browser: false,
+      },
+      rules: {strict: 'off'},
+    },
+  ],
+
+  rules: {
+    'no-unused-vars': ['error', {vars: 'all', args: 'all', argsIgnorePattern: '^_'}],
+    'no-shadow': 'error',
+    'prettier/prettier': 'error',
+  },
+};
